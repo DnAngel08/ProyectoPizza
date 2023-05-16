7355f506d1b0128e5bca70f3451ae02029c30ff5
DnAngel08 <cat.moon5@hotmail.com>
2023-05-15 19:23:16 -0600

    Tercer commit del proyecto

---
+++ BlazingPizza/Server/Controllers/ToppingsController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using BlazingPizza.Server.Models;
+using BlazingPizza.Shared;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlazingPizza.Server.Controllers
+{
+    [Route("toppings")]
+    [ApiController]
+    public class ToppingsController : ControllerBase
+    {
+        private readonly PizzaStoreContext Context;
+        public ToppingsController(PizzaStoreContext context)
+        {
+            this.Context = context;
+        }
+        [HttpGet]
+        public async Task<ActionResult<List<Topping>>> GetToppings()
+        {
+            return await Context.Toppings
+            .OrderBy(t => t.Name).ToListAsync();
+        }
+    }
+}
